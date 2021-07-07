46467ed6ccc7d1a373dd9b2826cbc7a59e7dff4f
rachanavishwanath <rachana.gujrathi@gmail.com>
2021-07-07 12:21:22 -0700

    Add package-json and webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/cloneTube.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+      extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            query: {
+                presets: ['@babel/env', '@babel/react']
+            }
+        },
+      }
+    ]
+  }
+};
